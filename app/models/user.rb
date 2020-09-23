0cd5b265eed70fde48a86121e2e49eb037f1d6c8
Jay Martens <martensjay93@gmail.com>
2020-09-23 14:22:19 -0400

    More testing config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
-
   has_secure_password
-  
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: {minimum: 8}
+  before_save :email_downcase
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password.strip)
+        return user
+    else 
+        return nil
+    end    
+  end
+  def email_downcase
+      self.email.downcase!
+  end
 end
